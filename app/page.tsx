cbb70e0fc317b42b7a44479b735bde93399032f4
CitrusSoda <dndhkaus1004@naver.com>
2024-06-20 18:33:21 +0900

    🎨 style: 테일윈드 공통 색상 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
-      <div className="size-[200px] bg-blue-500" />
-      <div className="size-[200px] bg-red-500" />
+      <div className="size-[200px] bg-violet-primary" />
+      <div className="size-[200px] bg-violet-secondary" />
+      <div className="flex flex-col items-center justify-center">
+        <div className="size-[50px] bg-white" />
+        <div className="size-[50px] bg-gray-100" />
+        <div className="size-[50px] bg-gray-200" />
+        <div className="size-[50px] bg-gray-300" />
+        <div className="size-[50px] bg-gray-400" />
+        <div className="size-[50px] bg-gray-500" />
+        <div className="size-[50px] bg-gray-600" />
+        <div className="size-[50px] bg-gray-700" />
+        <div className="size-[50px] bg-gray-800" />
+        <div className="size-[50px] bg-black" />
+      </div>
     </div>
   );
 }
