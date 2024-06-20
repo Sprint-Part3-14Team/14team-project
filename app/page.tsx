04eb6b99db1a02de7f6ec301ca4596388c0fc290
CitrusSoda <dndhkaus1004@naver.com>
2024-06-20 22:02:54 +0900

    🎨 style: 테일윈드 공통 색상 추가 및 pretendard 폰트 추가 (#2)
    * 🎨 style: 테일윈드 공통 색상 추가
    
    * 🎨 style: 폰트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="size-[200px] bg-blue-500" />
-      <div className="size-[200px] bg-red-500" />
+    // NOTE - 테스트 코드 입니다. 프로젝트 진행 시 편하게 삭제하셔도 됩니다.
+    <div className="flex flex-col items-center justify-center">
+      <div className="size-[200px] bg-violet-primary" />
+      <div className="size-[200px] bg-violet-secondary" />
+      <div className="flex flex-col items-center justify-center">
+        <div className="size-[50px] bg-white" />
+        <div className="size-[50px] bg-gray-100" />
+        <div className="size-[50px] bg-gray-200" />
+        <div className="size-[50px] bg-gray-300" />
+        <div className="size-[50px] bg-gray-400" />
+        <div className="size-[50px] bg-gray-500" />
+        <div className="size-[50px] bg-gray-600" />
+        <div className="size-[50px] bg-gray-700" />
+        <div className="size-[50px] bg-gray-800" />
+        <div className="size-[50px] bg-black" />
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="font-thin">Hello World!</h1>
+        <h1 className="font-extralight">Hello World!</h1>
+        <h1 className="font-light">Hello World!</h1>
+        <h1 className="font-normal">Hello World!</h1>
+        <h1 className="font-medium">Hello World!</h1>
+        <h1 className="font-semibold">Hello World!</h1>
+        <h1 className="font-bold">Hello World!</h1>
+        <h1 className="font-extrabold">Hello World!</h1>
+        <h1 className="font-black">Hello World!</h1>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,51 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        violet: {
+          primary: '#5534DA',
+          secondary: '#F1EFFD',
+        },
+        red: {
+          primary: '#D6173A',
+        },
+        green: {
+          primary: '#7AC555',
+        },
+        purple: {
+          primary: '#760DDE',
+        },
+        orange: {
+          primary: '#FFA500',
+        },
+        blue: {
+          primary: '#76A5EA',
+        },
+        pink: {
+          primary: '#E876EA',
+        },
+        gray: {
+          100: '#FAFAFA',
+          200: '#EEEEEE',
+          300: '#D9D9D9',
+          400: '#9FA6B2',
+          500: '#787486',
+          600: '#4B4B4B',
+          700: '#333236',
+          800: '#171717',
+        },
       },
     },
   },
