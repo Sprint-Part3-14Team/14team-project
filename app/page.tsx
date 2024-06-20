f3da794589bfdbece813f8c1440e904947286824
CitrusSoda <dndhkaus1004@naver.com>
2024-06-20 17:31:26 +0900

    🎉 begin: NextJS 앱라우터 설치 및 환경 설정 (#1)
    * 🔀 Squashed commit of the following:
    
    commit b2cad8038e3985b73b5998d8d5bd8f1b0996705c
    Author: CitrusSoda <dndhkaus1004@naver.com>
    Date:   Thu Jun 20 16:47:57 2024 +0900
    
        🎨 style: eslint 설정
    
        - airbnb + prettier 중복 설정 방지
    
    commit 939561b83170d2d548c067af8811b6b6f6c2df1a
    Author: CitrusSoda <dndhkaus1004@naver.com>
    Date:   Thu Jun 20 15:37:30 2024 +0900
    
        🎨 style: husky와 lint-staged 설정
    
    commit ebee296b23046f10e298e58396dd9bd91bf0daaf
    Author: CitrusSoda <dndhkaus1004@naver.com>
    Date:   Thu Jun 20 15:31:24 2024 +0900
    
        🎨 style: prettier 설정
    
    commit 85538157ba7a9f687817812dfaf46b11910995e3
    Author: CitrusSoda <dndhkaus1004@naver.com>
    Date:   Thu Jun 20 15:25:00 2024 +0900
    
        🎉 begin: NextJS 앱라우터 설치
    
    * 📦 chore: main에서 사라진 파일 복구

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="size-[200px] bg-blue-500" />
+      <div className="size-[200px] bg-red-500" />
+    </div>
+  );
+}
