35fc65d11c3f7ae252ca78e7f170683f669eeceb
haze <skdml20022@gmail.com>
2024-06-23 05:25:27 +0900

    💄 design: 반응형 스타일 추가'

--- app/components/landing_components/ad-card.tsx
+++ app/components/landing_components/ad-card.tsx
@@ -14,29 +14,28 @@ export default function AdCard({
   description,
 }: AdCardProps) {
   const imgContainerYPadding: { [key: string]: string } = {
-    dashboard: 'py-[64px]',
-    invite: 'py-[18px]',
-    member: 'py-[33px]',
+    dashboard: 'py-16 md:py-[68px]',
+    invite: 'py-5 md:py-[15px]',
+    member: 'py-[33px] md:py-[32px]',
   };
 
   return (
-    <div className="w-full">
+    <div className="w-full md:w-[378px]">
       <div
-        className={`relative h-[260px] rounded-t-lg bg-gray-600 px-10 ${imgContainerYPadding[yPadding]}`}
+        className={`h-[236px] rounded-t-lg bg-gray-600 px-10 md:h-[260px] md:px-[39px] ${imgContainerYPadding[yPadding]}`}
       >
         <Image
           src={imgUrl}
           alt={`${title} 구간 이미지`}
           width={0}
           height={0}
-          sizes="100vw"
           style={{
             width: '100%',
             height: 'auto',
           }}
         />
       </div>
-      <div className="h-[124px] rounded-b-lg bg-gray-800 py-[27px] pl-[32px] text-white">
+      <div className="h-[124px] rounded-b-lg bg-gray-800 py-[27px] pl-[32px] text-white md:py-[33px]">
         <p className="mb-[18px] text-lg font-bold">{title}</p>
         <p className="text-base font-medium">{description}</p>
       </div>
