9442748f8d9e568b1c6c51c5dd7a4da8a3b67b3b
CitrusSoda <dndhkaus1004@naver.com>
2024-06-23 17:06:38 +0900

    💄 design: lg였던 브레이크 포인트를 xl로 수정

--- app/(dashboard)/_components/dashboard-card.tsx
+++ app/(dashboard)/_components/dashboard-card.tsx
@@ -18,20 +18,20 @@ export default function DashboardCard({
   };
 }) {
   return (
-    <div className="mt-[10px] flex flex-col rounded-md border border-gray-300 bg-white p-3 md:flex-row md:items-center md:gap-x-5 lg:w-[314px] lg:flex-col">
+    <div className="mt-[10px] flex flex-col rounded-md border border-gray-300 bg-white p-3 md:flex-row md:items-center md:gap-x-5 xl:w-[314px] xl:flex-col">
       {imageUrl && (
-        <div className="relative h-[150px] rounded-md bg-gray-100 md:h-[53px] md:w-[90px] lg:h-[160px] lg:w-full">
+        <div className="relative h-[150px] rounded-md bg-gray-100 md:h-[53px] md:w-[90px] xl:h-[160px] xl:w-full">
           <Image src={imageUrl} alt="card-image" fill />
         </div>
       )}
-      <div className="mt-[10px] flex w-full flex-col md:mt-0 md:gap-y-[10px] lg:mt-3 lg:gap-y-0">
+      <div className="mt-[10px] flex w-full flex-col md:mt-0 md:gap-y-[10px] xl:mt-3 xl:gap-y-0">
         <p className="font-medium">{title}</p>
-        <div className="w-full md:flex lg:flex-col">
-          <div className="my-[6px] flex gap-x-[6px] md:my-0 lg:my-[10px]">
+        <div className="w-full md:flex xl:flex-col">
+          <div className="my-[6px] flex gap-x-[6px] md:my-0 xl:my-[10px]">
             {tags?.map((tag) => <p key={tag}>{tag}</p>)}
           </div>
           <div className="flex w-full justify-between">
-            <div className="flex items-center gap-x-[6px] md:ml-4 lg:ml-0">
+            <div className="flex items-center gap-x-[6px] md:ml-4 xl:ml-0">
               <Image
                 src="/icons/icon_calendar.svg"
                 width={16}
