5a5d4eb506da50306512398adf281aaa145acf50
seo02wow <rlatjdud9188@naver.com>
2024-06-24 14:34:49 +0900

    :lipstick:design: /mydashboard로 이동하는 로고 생성

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,7 +1,23 @@
+import logoSmall from '@/public/images/logo_small.svg';
+import textLogo from '@/public/images/text_logo.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-10 h-[60px] border-b border-gray-300 bg-white md:h-[70px]">
-      네비게이션바
+      <div>
+        <Link href="/mydashboard">
+          <div className="flex items-center">
+            <div className="relative h-[34px] w-[29px]">
+              <Image src={logoSmall} fill alt="테스키파이 로고" />
+            </div>
+            <div className="relative h-[22px] w-20">
+              <Image src={textLogo} alt="테스키파이 텍스트 로고" fill />
+            </div>
+          </div>
+        </Link>
+      </div>
     </header>
   );
 }
