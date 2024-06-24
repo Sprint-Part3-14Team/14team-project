d88918d2c7bc4a78b4ee3249a7609dc44986d234
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2024-06-24 17:26:20 +0900

    💄design: #12 헤더 회원 정보 추가 (#40)
    * :fire:remove: 사이드바 프로필 및 로고 삭제
    
    * :lipstick:design: /mydashboard로 이동하는 로고 생성
    
    * :lipstick:design: 헤더 모바일 UI 구현
    
    * :lipstick:design: 헤더 모바일 태블릿, PC 구현
    
    * :lipstick:design: 네비게이션 헤더 높이만큼 margin top 설정

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,7 +1,39 @@
+import logoSmall from '@/public/images/logo_small.svg';
+import textLogo from '@/public/images/text_logo.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import ProfileImage from '../profile/profile-image';
+
 export default function Navbar() {
+  // NOTE - 테스트 유저 정보
+  const nickname = '서영';
+  const profileImageUrl = null;
+  const id = 3950;
+
   return (
     <header className="sticky top-0 z-10 h-[60px] border-b border-gray-300 bg-white md:h-[70px]">
-      네비게이션바
+      <div className="flex items-center justify-between px-3 py-[13px] md:px-10 md:py-4">
+        <Link href="/mydashboard">
+          <div className="flex items-center">
+            <div className="relative h-[34px] w-[29px]">
+              <Image src={logoSmall} fill alt="테스키파이 로고" />
+            </div>
+            <div className="relative h-[22px] w-20">
+              <Image src={textLogo} alt="테스키파이 텍스트 로고" fill />
+            </div>
+          </div>
+        </Link>
+        <div className="flex items-center gap-3 text-base font-medium">
+          <ProfileImage
+            nickname={nickname}
+            profileImageUrl={profileImageUrl}
+            id={id}
+            size="34px"
+          />
+          <p className="hidden text-gray-700 md:block">{nickname}</p>
+        </div>
+      </div>
     </header>
   );
 }
--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -1,31 +1,12 @@
 import addDashboard from '@/public/icons/add_dashboard.svg';
-import logoSmall from '@/public/images/logo_small.svg';
-import textLogo from '@/public/images/text_logo.svg';
 import Image from 'next/image';
-import Link from 'next/link';
 
 import SidebarDashboardList from './sidebar-dashboard-list';
-import SidebarProfile from './sidebar-profile';
 
 export default function Sidebar() {
   return (
     <aside className="flex h-full w-[67px] flex-shrink-0 flex-col border-r border-gray-300 md:w-40 xl:w-[300px]">
-      <div className="my-5 flex flex-col items-center justify-center md:mx-[14px] xl:items-stretch">
-        <Link href="/dashboard" className="mb-[38px] md:mb-14 xl:mb-[60px]">
-          <div className="flex items-center">
-            <Image
-              src={logoSmall}
-              width={24}
-              height={28}
-              alt="테스키파이 로고"
-            />
-            <div className="relative hidden h-[22px] w-20 md:block">
-              <Image src={textLogo} alt="테스키파이 텍스트 로고" fill />
-            </div>
-          </div>
-        </Link>
-        <SidebarProfile />
-        <section />
+      <div className="mt-[60px] flex flex-col items-center justify-center md:mx-[14px] md:mt-[70px] xl:items-stretch">
         <div className="mb-[38px] flex items-center justify-between md:mb-[30px] md:w-full">
           <p className="flex hidden text-xs font-bold text-gray-500 md:block">
             Dash Boards
