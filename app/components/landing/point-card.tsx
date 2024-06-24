e68031772520ef44ed005f80cb95c3311b710763
haze <skdml20022@gmail.com>
2024-06-24 17:51:30 +0900

    💄 design: point-card 좌우 반전 스타일 처리

--- app/components/landing/point-card.tsx
+++ app/components/landing/point-card.tsx
@@ -2,23 +2,28 @@ import { ReactNode } from 'react';
 
 interface PointCardProps {
   pointNum: number;
-  description: string;
+  firstDesc: string;
+  secondDesc: string;
   children: ReactNode;
+  isReverse: boolean;
 }
 
 export default function PointCard({
   pointNum,
-  description,
+  firstDesc,
+  secondDesc,
   children,
+  isReverse = false,
 }: PointCardProps) {
   return (
-    <div className="relative h-[686px] w-full rounded-lg bg-gray-800 pt-[60px] text-center md:h-[972px] md:text-left xl:h-[600px]">
-      <h2 className="text-lg font-medium text-gray-400 md:ml-[60px]">
-        Point {pointNum}
-      </h2>
-      <p className="mx-[60px] mb-[100px] mt-[60px] break-keep text-[36px] font-bold leading-[50px] md:mt-[100px] md:h-[128px] md:w-[302px] md:text-[48px] md:leading-[64px]">
-        {description}
-      </p>
+    <div className="relative h-[686px] w-full rounded-lg bg-gray-800 pt-[60px] text-center md:h-[972px] md:text-left xl:h-[600px] xl:pt-[123px]">
+      <div className={`${isReverse ? 'xl:ml-[580px]' : ''} md:ml-[60px]`}>
+        <h2 className="text-lg font-medium text-gray-400">Point {pointNum}</h2>
+        <div className="mb-[100px] mt-[60px] text-[36px] font-bold leading-[50px] md:text-[48px] md:leading-[64px] xl:mt-[100px]">
+          <p>{firstDesc}</p>
+          <p>{secondDesc}</p>
+        </div>
+      </div>
       {children}
     </div>
   );
