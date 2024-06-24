15dcd0b461eed850945d1db44ad54da672aa1656
seo02wow <rlatjdud9188@naver.com>
2024-06-24 17:14:21 +0900

    :lipstick:design: 네비게이션 헤더 높이만큼 margin top 설정

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -6,8 +6,7 @@ import SidebarDashboardList from './sidebar-dashboard-list';
 export default function Sidebar() {
   return (
     <aside className="flex h-full w-[67px] flex-shrink-0 flex-col border-r border-gray-300 md:w-40 xl:w-[300px]">
-      <div className="my-5 flex flex-col items-center justify-center md:mx-[14px] xl:items-stretch">
-        <section />
+      <div className="mt-[60px] flex flex-col items-center justify-center md:mx-[14px] md:mt-[70px] xl:items-stretch">
         <div className="mb-[38px] flex items-center justify-between md:mb-[30px] md:w-full">
           <p className="flex hidden text-xs font-bold text-gray-500 md:block">
             Dash Boards
