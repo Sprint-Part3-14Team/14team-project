f1213801ed107f336ba2a813bffc009bd7f05e7d
seo02wow <rlatjdud9188@naver.com>
2024-06-24 16:40:06 +0900

    :lipstick:design: 헤더 모바일 UI 구현

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -3,10 +3,17 @@ import textLogo from '@/public/images/text_logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import ProfileImage from '../profile/profile-image';
+
 export default function Navbar() {
+  // NOTE - 테스트 유저 정보
+  const nickname = '서영';
+  const profileImageUrl = null;
+  const id = 3950;
+
   return (
     <header className="sticky top-0 z-10 h-[60px] border-b border-gray-300 bg-white md:h-[70px]">
-      <div>
+      <div className="flex items-center justify-between px-3 py-[13px]">
         <Link href="/mydashboard">
           <div className="flex items-center">
             <div className="relative h-[34px] w-[29px]">
@@ -17,6 +24,13 @@ export default function Navbar() {
             </div>
           </div>
         </Link>
+        <ProfileImage
+          nickname={nickname}
+          profileImageUrl={profileImageUrl}
+          id={id}
+          size="34px"
+        />
+        <p className="hidden">{nickname}</p>
       </div>
     </header>
   );
