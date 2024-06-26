774c46067d4d5d6405f1b8377b24c5ede10d8dca
seo02wow <rlatjdud9188@naver.com>
2024-06-27 00:17:04 +0900

    :sparkles:feat: 대시보드 정보 및 대시보드 참여 멤버 목록 데이터 패칭

--- app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/dashboard-navbar.tsx
+++ app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/dashboard-navbar.tsx
@@ -1,7 +1,24 @@
-export default function DashboardNavbar() {
+import getFetcher from '@/lib/api/getFetcher';
+import { Dashboard } from '@/types/dashboard';
+
+export default async function DashboardNavbar({ id }: { id: number }) {
+  const data = (await getFetcher(`/dashboards/${id}`)) as Dashboard;
+
+  const params = new URLSearchParams({
+    dashboardId: id.toString(),
+    page: '1',
+    size: '10',
+  });
+
+  // NOTE - 대시보드 참여 멤버 목록
+  const { members } = await getFetcher(`/members?${params.toString()}`);
+  console.log(members);
+
   return (
     <section className="sticky top-[60px] z-[9] h-[60px] border-b border-gray-300 bg-white md:top-[70px]">
-      <div>DashboardNavbar</div>
+      <h2>{data.title}</h2>
+      {/* TODO - 관리버튼 추가 */}
+      {data.createdByMe && <p>관리</p>}
     </section>
   );
 }
