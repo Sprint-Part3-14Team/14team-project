25344de33cb489872bbb4cfe66ef6c4c32fe57e4
seo02wow <rlatjdud9188@naver.com>
2024-06-26 11:25:27 +0900

    :lipstick:design: 대시보드 카드 모바일 UI 구현

--- app/(dashboard)/dashboard/[id]/page.tsx
+++ app/(dashboard)/dashboard/[id]/page.tsx
@@ -33,69 +33,69 @@ const dummyCardsOne: Cards[] = [
       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png',
     createdAt: new Date(),
   },
-  // {
-  //   id: 4,
-  //   title: 'Card 4',
-  //   tags: ['tag7', 'tag8', 'tag9'],
-  //   assignee: {
-  //     profileImageUrl: '',
-  //     nickname: 'User4',
-  //     id: 4,
-  //   },
-  //   imageUrl:
-  //     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
-  //   createdAt: new Date(),
-  // },
-  // {
-  //   id: 6,
-  //   title: 'Card 6',
-  //   tags: ['tag11', 'tag12'],
-  //   assignee: {
-  //     profileImageUrl: '',
-  //     nickname: 'User6',
-  //     id: 6,
-  //   },
-  //   imageUrl: '',
-  //   createdAt: new Date(),
-  // },
-  // {
-  //   id: 8,
-  //   title: 'Card 8',
-  //   tags: ['tag15', 'tag16'],
-  //   assignee: {
-  //     profileImageUrl: '',
-  //     nickname: 'User8',
-  //     id: 8,
-  //   },
-  //   imageUrl:
-  //     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/389.png',
-  //   createdAt: new Date(),
-  // },
-  // {
-  //   id: 10,
-  //   title: 'Card 10',
-  //   tags: ['tag19', 'tag20'],
-  //   assignee: {
-  //     profileImageUrl: '',
-  //     nickname: 'User10',
-  //     id: 10,
-  //   },
-  //   imageUrl: '',
-  //   createdAt: new Date(),
-  // },
-  // {
-  //   id: 10,
-  //   title: 'Card 10',
-  //   tags: ['tag19', 'tag20'],
-  //   assignee: {
-  //     profileImageUrl: '',
-  //     nickname: 'User10',
-  //     id: 10,
-  //   },
-  //   imageUrl:
-  //     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
-  //   createdAt: new Date(),
-  // },
+  {
+    id: 4,
+    title: 'Card 4',
+    tags: ['tag7', 'tag8', 'tag9'],
+    assignee: {
+      profileImageUrl: '',
+      nickname: 'User4',
+      id: 4,
+    },
+    imageUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+    createdAt: new Date(),
+  },
+  {
+    id: 6,
+    title: 'Card 6',
+    tags: ['tag11', 'tag12'],
+    assignee: {
+      profileImageUrl: '',
+      nickname: 'User6',
+      id: 6,
+    },
+    imageUrl: '',
+    createdAt: new Date(),
+  },
+  {
+    id: 8,
+    title: 'Card 8',
+    tags: ['tag15', 'tag16'],
+    assignee: {
+      profileImageUrl: '',
+      nickname: 'User8',
+      id: 8,
+    },
+    imageUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/389.png',
+    createdAt: new Date(),
+  },
+  {
+    id: 10,
+    title: 'Card 10',
+    tags: ['tag19', 'tag20'],
+    assignee: {
+      profileImageUrl: '',
+      nickname: 'User10',
+      id: 10,
+    },
+    imageUrl: '',
+    createdAt: new Date(),
+  },
+  {
+    id: 10,
+    title: 'Card 10',
+    tags: ['tag19', 'tag20'],
+    assignee: {
+      profileImageUrl: '',
+      nickname: 'User10',
+      id: 10,
+    },
+    imageUrl:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
+    createdAt: new Date(),
+  },
 ];
 
 const dummyCardsTwo: Cards[] = [
--- app/(dashboard)/mydashboard/_components/dashboards.tsx
+++ app/(dashboard)/mydashboard/_components/dashboards.tsx
@@ -1,5 +1,7 @@
 import getFetcher from '@/lib/api/getFetcher';
+import crown from '@/public/icons/crown_icon.svg';
 import { Dashboard } from '@/types/dashboard';
+import Image from 'next/image';
 
 export default async function DashBoards() {
   const params = new URLSearchParams({
@@ -11,12 +13,24 @@ export default async function DashBoards() {
   const data = await getFetcher(`/dashboards?${params.toString()}`);
   const { dashboards } = data;
 
+  // NOTE - 사용자 정보 GET
+
   return (
     <section>
-      <ul className="">
+      <ul className="flex flex-col gap-2">
         {dashboards.map((dashboard: Dashboard) => (
-          <li>
+          <li className="h-20[90px] relative flex rounded-lg border border-gray-300 bg-white py-5 pl-5">
+            <div
+              className="absolute left-0 top-[50%] h-7 w-1 translate-y-[-50%] rounded-r-lg"
+              style={{ backgroundColor: dashboard.color }}
+             />
             <p>{dashboard.title}</p>
+            {/* // NOTE - 만든 사람 프로필 넣기 불가능함 내가 만든 대시보드일 경우 왕관 OR 프로필 이미지(없으면 기본 프로필 이미지) */}
+            {dashboard.createdByMe && (
+              <div className="relative h-3 w-4">
+                <Image src={crown} alt="내가 만든 대시보드 왕관" fill />
+              </div>
+            )}
           </li>
         ))}
       </ul>
