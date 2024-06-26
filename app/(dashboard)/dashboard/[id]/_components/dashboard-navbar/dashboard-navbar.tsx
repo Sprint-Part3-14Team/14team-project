bc77399b2baef803e182dfda23e584d87760477c
seo02wow <rlatjdud9188@naver.com>
2024-06-27 00:47:20 +0900

    :lipstick:design: 사이드바 영역 디자인 구현

--- app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/dashboard-navbar.tsx
+++ app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/dashboard-navbar.tsx
@@ -1,3 +1,4 @@
+import IconButtons from '@/app/components/icon-buttons';
 import getFetcher from '@/lib/api/getFetcher';
 import { Dashboard } from '@/types/dashboard';
 
@@ -11,14 +12,28 @@ export default async function DashboardNavbar({ id }: { id: number }) {
   });
 
   // NOTE - 대시보드 참여 멤버 목록
-  const { members } = await getFetcher(`/members?${params.toString()}`);
+  const { members, totalCount } = await getFetcher(
+    `/members?${params.toString()}`
+  );
+  // TODO - ESLint 우회, 지우기
   console.log(members);
+  console.log(totalCount);
 
   return (
-    <section className="sticky top-[60px] z-[9] h-[60px] border-b border-gray-300 bg-white md:top-[70px]">
-      <h2>{data.title}</h2>
-      {/* TODO - 관리버튼 추가 */}
-      {data.createdByMe && <p>관리</p>}
+    <section className="sticky top-[60px] z-[9] flex h-[60px] items-center justify-between border-b border-gray-300 bg-white px-3 py-1 md:top-[70px] md:px-10">
+      <div className="flex items-center gap-1">
+        <h2 className="text-lg font-bold">{data.title}</h2>
+        {/* TODO - 관리버튼 추가 */}
+        {data.createdByMe && <IconButtons variant="setting" />}{' '}
+      </div>
+      <div className="flex items-center gap-1">
+        {/* TODO - 참여 멤버 컴포넌트 우선 위치 잡기 위해 넣어둔 겁니다 ! */}
+        <div className="flex">
+          <div className="size-[26px] rounded-full bg-red-400">서</div>
+          <div className="size-[26px] rounded-full bg-red-400">서</div>
+        </div>
+        <IconButtons variant="dashboard" />
+      </div>
     </section>
   );
 }
