2712e28d75f12827ba2840fd3f8995f09c313179
seo02wow <rlatjdud9188@naver.com>
2024-06-26 12:22:37 +0900

    :recycle:refactor: 대시보드 카드 컴포넌트 분리 및 대시보드 제목, 개수 추가

--- app/(dashboard)/mydashboard/_components/dashboards.tsx
+++ app/(dashboard)/mydashboard/_components/dashboards.tsx
@@ -1,7 +1,8 @@
-import ProfileImage from '@/app/components/profile/profile-image';
 import getFetcher from '@/lib/api/getFetcher';
 import getUserMeFetcher from '@/lib/api/getUserMeFetcher';
 import { Dashboard } from '@/types/dashboard';
+
+import DashboardCard from './dashboard-card';
 
 export default async function DashBoards() {
   const params = new URLSearchParams({
@@ -11,33 +12,20 @@ export default async function DashBoards() {
   });
 
   const data = await getFetcher(`/dashboards?${params.toString()}`);
-  const { dashboards } = data;
+  const { dashboards, totalCount } = data;
 
   // NOTE - 사용자 정보 GET
   const user = await getUserMeFetcher();
 
   return (
     <section>
-      <p>{user.id}</p>
+      <div className="mb-3 flex items-center gap-1 text-xs">
+        <h2 className="text-xl font-bold">Dashboard</h2>
+        <p>{totalCount}</p>
+      </div>
       <ul className="flex flex-col gap-2">
         {dashboards.map((dashboard: Dashboard) => (
-          <li className="h-20[90px] relative flex items-center justify-between rounded-lg border border-gray-300 bg-white px-5 py-5">
-            <div
-              className="absolute left-0 top-[50%] h-7 w-1 translate-y-[-50%] rounded-r-lg"
-              style={{ backgroundColor: dashboard.color }}
-            />
-            <p className="text-sm font-semibold text-gray-700">
-              {dashboard.title}
-            </p>
-            {dashboard.createdByMe && (
-              <ProfileImage
-                nickname={user.nickname}
-                profileImageUrl={user.profileImageUrl}
-                id={user.id}
-                size="20px"
-              />
-            )}
-          </li>
+          <DashboardCard dashboard={dashboard} user={user} />
         ))}
       </ul>
     </section>
