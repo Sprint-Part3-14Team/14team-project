1d57bd3384cc3aebf52362fb1f290d886aeea275
JHmeatschool <ceojr1234@gmail.com>
2024-06-27 23:23:02 +0900

    ♻ refactor: 테스트 코드 수정

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,22 +1,8 @@
-'use client';
-
-import NewDashboardModal from '@/app/(dashboard)/mydashboard/_components/new-dashboard-modal';
 import Link from 'next/link';
-import React, { useState } from 'react';
 
-import SignModal from '../_components/auth-modal';
 import SignInForm from '../_components/sign-in-form';
 
 export default function Login() {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [isTestModalOpen, setTestModalOpen] = useState(false);
-
-  const openModal = () => setModalOpen(true);
-  const closeModal = () => setModalOpen(false);
-
-  const openTestModal = () => setTestModalOpen(true);
-  const closeTestModal = () => setTestModalOpen(false);
-
   return (
     <div className="flex w-full flex-col items-center justify-center px-3 md:px-28 xl:mx-auto xl:w-[520px] xl:px-0">
       <p className="mb-10 font-medium">오늘도 만나서 반가워요!</p>
@@ -27,22 +13,6 @@ export default function Login() {
           회원가입하기
         </Link>
       </p>
-      {/* //NOTE - 테스트용 */}
-      <div>
-        <button type="button" onClick={openModal}>
-          모달 열기
-        </button>
-        <SignModal
-          isOpen={isModalOpen}
-          onClose={closeModal}
-          variant="passwordError"
-        />
-      </div>
-
-      <button type="button" onClick={openTestModal}>
-        모달 열기
-      </button>
-      <NewDashboardModal isOpen={isTestModalOpen} onClose={closeTestModal} />
     </div>
   );
 }
