d4b143f0ceef1374f65b910519a1626ab1ed066a
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2024-06-27 20:35:51 +0900

    ✨feat: 대시보드 멤버 리스트 (#76)
    * :sparkles:feat: DashboardMemberList 컴포넌트 props 내려주기
    
    * :sparkles:feat: 대시보드 참여목록 컴포넌트 모바일, 태블릿 구현
    
    * :package:chore: react-responsive 설치
    
    * :sparkles:feat: 대시보드 참여목록 PC에서 4개 보이는 기능 추가
    
    * :recycle:refactor: 헤더 테스트 데이터 삭제 후 데이터 패칭
    
    * :lipstick:design: 프로필 border 추가
    
    * :bug:fix: 프로필 이미지 프롭 userId로 변경하여 색상 바뀌는 문제 해결

---
+++ app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/dashboard-member-list.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import ProfileImage from '@/app/components/profile/profile-image';
+import { DashboardMembers } from '@/types/members';
+import { useMediaQuery } from 'react-responsive';
+
+interface DashboardMembersProps {
+  members: DashboardMembers[];
+  totalCount: number;
+}
+
+export default function DashboardMemberList({
+  members,
+  totalCount,
+}: DashboardMembersProps) {
+  const isPC = useMediaQuery({ minWidth: 1280 });
+  const displayMembers = isPC ? members.slice(0, 4) : members.slice(0, 2);
+  const remainCount = totalCount - displayMembers.length;
+
+  return (
+    <div className="flex">
+      {displayMembers.map((member) => (
+        <div key={member.id} className="ml-[-8px]">
+          <ProfileImage
+            profileImageUrl={member.profileImageUrl}
+            nickname={member.nickname}
+            id={member.userId}
+            size="26px"
+          />
+        </div>
+      ))}
+      {remainCount > 0 && (
+        <div className="ml-[-8px] flex size-[26px] items-center justify-center rounded-full border-2 border-white bg-[#F4D7DA] text-sm text-white">
+          +{remainCount}
+        </div>
+      )}
+    </div>
+  );
+}
--- app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/dashboard-navbar.tsx
+++ app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/dashboard-navbar.tsx
@@ -2,6 +2,8 @@ import IconButtons from '@/app/components/icon-buttons';
 import getFetcher from '@/lib/api/getFetcher';
 import { Dashboard } from '@/types/dashboard';
 
+import DashboardMemberList from './dashboard-member-list';
+
 export default async function DashboardNavbar({ id }: { id: number }) {
   const data = (await getFetcher(`/dashboards/${id}`)) as Dashboard;
 
@@ -15,9 +17,6 @@ export default async function DashboardNavbar({ id }: { id: number }) {
   const { members, totalCount } = await getFetcher(
     `/members?${params.toString()}`
   );
-  // TODO - ESLint 우회, 지우기
-  console.log(members);
-  console.log(totalCount);
 
   return (
     <section className="sticky top-[60px] z-[9] flex h-[60px] items-center justify-between border-b border-gray-300 bg-white px-3 py-1 md:top-[70px] md:px-10">
@@ -26,12 +25,7 @@ export default async function DashboardNavbar({ id }: { id: number }) {
         {data.createdByMe && <IconButtons variant="setting" />}
       </div>
       <div className="flex items-center gap-1">
-        {/* TODO - 참여 멤버 컴포넌트 생성 */}
-        {/* NOTE -  우선 위치 잡기 위해 넣어둔 겁니다 ! */}
-        <div className="flex">
-          <div className="size-[26px] rounded-full bg-red-400">서</div>
-          <div className="size-[26px] rounded-full bg-red-400">서</div>
-        </div>
+        <DashboardMemberList members={members} totalCount={totalCount} />
         <IconButtons variant="dashboard" />
       </div>
     </section>
