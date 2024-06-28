cec75dbbcd6a31b72e311bf222ce3237b066455d
seo02wow <rlatjdud9188@naver.com>
2024-06-28 11:18:34 +0900

    :lipstick:design: 초대받은 대시보드 버튼 제외한 디자인 구현

--- app/(dashboard)/mydashboard/_components/invitation-card.tsx
+++ app/(dashboard)/mydashboard/_components/invitation-card.tsx
@@ -1,3 +1,29 @@
-export default function InvitationCard() {
-  return <div />;
+import { Invitation } from '@/types/invitations';
+
+interface InvitationCardProps {
+  invitation: Invitation;
+}
+
+export default function InvitationCard({ invitation }: InvitationCardProps) {
+  return (
+    <li
+      key={invitation.id}
+      className="flex flex-col border-b border-gray-200 px-4 py-4"
+    >
+      <div className="flex flex-col gap-[10px]">
+        <div className="flex gap-7">
+          <p className="text-sm font-normal text-gray-400">이름</p>
+          <p className="text-sm font-normal text-gray-700">
+            {invitation.dashboard.title}
+          </p>
+        </div>
+        <div className="flex gap-7">
+          <p className="text-sm font-normal text-gray-400">초대자</p>
+          <p className="text-sm font-normal text-gray-700">
+            {invitation.invitee.nickname}
+          </p>
+        </div>
+      </div>
+    </li>
+  );
 }
--- app/(dashboard)/mydashboard/_components/invitation-list.tsx
+++ app/(dashboard)/mydashboard/_components/invitation-list.tsx
@@ -1,5 +1,7 @@
 import getFetcher from '@/lib/api/getFetcher';
-import { Invitations } from '@/types/invitations';
+import { Invitation, Invitations } from '@/types/invitations';
+
+import InvitationCard from './invitation-card';
 
 export default async function InvitationList() {
   // TODO - 무한 스크롤, 검색 cursorId,title
@@ -11,8 +13,15 @@ export default async function InvitationList() {
     `/invitations?${params.toString()}`
   );
   const { invitations } = data;
-  console.log('>>>>>>>>>>>>>>>>>>>>>>>>초대 리스트');
-  console.log(invitations);
+  // NOTE - inviteAccepted null인 경우만 조회됨
 
-  return <div />;
+  return (
+    <div>
+      <ul>
+        {invitations.map((invitation: Invitation) => (
+          <InvitationCard invitation={invitation} />
+        ))}
+      </ul>
+    </div>
+  );
 }
