c69abb1336b6f9cf0ad8a6e962dc72c3f2cbe156
seo02wow <rlatjdud9188@naver.com>
2024-06-28 10:11:23 +0900

    :lipstick:design: input 및 제목 디자인

--- app/(dashboard)/mydashboard/_components/invitations.tsx
+++ app/(dashboard)/mydashboard/_components/invitations.tsx
@@ -1,3 +1,14 @@
 export default function Invitaions() {
-  return <section>초대 받은 대시보드 리스트</section>;
+  return (
+    <section className="mt-6 bg-white px-4 py-6 md:px-7 md:py-8">
+      <h2 className="mb-5 text-xl font-bold text-gray-700">
+        초대받은 대시보드
+      </h2>
+      <input
+        type="text"
+        className="h-9 w-full rounded-md border border-gray-300 py-[10px]"
+        placeholder="검색"
+      />
+    </section>
+  );
 }
