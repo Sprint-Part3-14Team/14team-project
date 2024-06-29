7f9675b9b86f973b7244ea874c788c1e8cf35cbb
seo02wow <rlatjdud9188@naver.com>
2024-06-29 14:49:16 +0900

    :sparkles:feat: 초대 응답 기능 구현
    - 초대 응답 기능 구현
    - 초대 응답 후 데이터 중복 오류 해결

--- app/(dashboard)/mydashboard/page.tsx
+++ app/(dashboard)/mydashboard/page.tsx
@@ -1,11 +1,11 @@
 import DashBoards from './_components/dashboards';
-import Invitaions from './_components/invitations';
+import Invitations from './_components/invitations';
 
 export default function MyDashboard() {
   return (
     <main className="h-[calc(100vh-60px)] max-w-[1022px] overflow-auto p-6 md:h-[calc(100vh-70px)] md:p-10">
       <DashBoards />
-      <Invitaions />
+      <Invitations />
     </main>
   );
 }
