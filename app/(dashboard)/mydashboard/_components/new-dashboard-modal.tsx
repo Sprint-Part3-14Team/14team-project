18b03455aee51e4cfde0a50011515d52bbfd5472
JHmeatschool <162143999+JHmeatschool@users.noreply.github.com>
2024-06-29 17:15:25 +0900

    feat: 대시보드 생성 모달 구현  (#93)
    * ✨ feat: 대시보드 생성 모달 틀 생성
    
    * ♻ refactor: 테스트 코드 수정
    
    * ✨ feat: postFetcher 추가
    
    * ✨ feat: 이름 색상 선택후 생성버튼 활성화
    
    * ♻ refactor: 토큰 수정
    
    * ♻ refactor: 코드 수정

--- app/(dashboard)/mydashboard/_components/new-dashboard-modal.tsx
+++ app/(dashboard)/mydashboard/_components/new-dashboard-modal.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import Buttons from '@/app/components/button';
 import ColorList from '@/app/components/color-list';
 import Modal from '@/app/components/modal';
+import { TEAM_BASE_URL } from '@/constants/TEAM_BASE_URL';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 interface NewDashboardModalProps {
   isOpen: boolean;
@@ -11,6 +16,36 @@ export default function NewDashboardModal({
   isOpen,
   onClose,
 }: NewDashboardModalProps) {
+  const [dashboardName, setDashboardName] = useState('');
+  const [selectedColor, setSelectedColor] = useState('');
+  const router = useRouter();
+
+  const handleCreateDashboard = async () => {
+    try {
+      if (!dashboardName || !selectedColor) return;
+
+      const res = await fetch(`${TEAM_BASE_URL}/dashboards`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // NOTE - 토큰 추가 예정
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mzk1MCwidGVhbUlkIjoiNi0xNCIsImlhdCI6MTcxOTA2NjU1OCwiaXNzIjoic3AtdGFza2lmeSJ9.P1BK3gMqx09fVNkM93D45YjpxHfXTsg55IpQFNBKan0`,
+        },
+        body: JSON.stringify({ title: dashboardName, color: selectedColor }),
+      });
+
+      if (!res.ok) {
+        throw new Error('대시보드 생성 실패');
+      }
+
+      const createdDashboard = await res.json();
+      router.push(`/dashboard/${createdDashboard.id}`);
+      onClose();
+    } catch (error: any) {
+      console.error('대시보드 생성 오류:', error.message);
+    }
+  };
+
   if (!isOpen) return null;
 
   return (
@@ -22,7 +57,12 @@ export default function NewDashboardModal({
       <h2 className="ml-[20px] mt-[28px] text-xl font-bold md:ml-[28px] md:mt-[32px] md:text-2xl">
         새로운 대시보드
       </h2>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleCreateDashboard();
+        }}
+      >
         <label
           htmlFor="dashboardName"
           className="ml-[20px] mt-[24px] block text-base font-medium text-gray-700 md:ml-[28px] md:mt-[32px] md:text-lg"
@@ -34,12 +74,14 @@ export default function NewDashboardModal({
             id="title"
             type="text"
             placeholder="대시보드 이름"
+            value={dashboardName}
+            onChange={(e) => setDashboardName(e.target.value)}
             className="mt-[10px] flex h-[42px] w-[287px] rounded-md border border-gray-300 pl-[16px] text-sm font-normal text-gray-700 md:h-[48px] md:w-[484px] md:text-base"
           />
         </div>
         <ColorList
           className="ml-[20px] mt-[24px] flex md:ml-[28px] md:mt-[28px]"
-          register={() => {}}
+          register={(color: string) => setSelectedColor(color)}
         />
         <div className="mt-[76px] flex justify-center md:mt-[86px] md:justify-end">
           <Buttons
@@ -52,7 +94,8 @@ export default function NewDashboardModal({
           <Buttons
             variant="mobile138x42"
             type="submit"
-            className="ml-[12px] rounded-lg bg-violet-primary text-white md:mr-7"
+            className={`ml-[12px] rounded-lg bg-violet-primary text-white md:mr-[28px] ${dashboardName && selectedColor ? 'cursor-default' : 'cursor-not-allowed'}`}
+            disabled={!dashboardName || !selectedColor}
           >
             생성
           </Buttons>
--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -1,6 +1,4 @@
-import addDashboard from '@/public/icons/add_dashboard.svg';
-import Image from 'next/image';
-
+import SidebarButton from './sidebar-button';
 import SidebarDashboardList from './sidebar-dashboard-list';
 
 export default function Sidebar() {
@@ -8,12 +6,10 @@ export default function Sidebar() {
     <aside className="flex h-screen w-[67px] flex-shrink-0 flex-col border-r border-gray-300 md:w-40 xl:w-[300px]">
       <div className="mt-[60px] flex flex-col items-center justify-center md:mx-[14px] md:mt-[70px] xl:items-stretch">
         <div className="mb-[38px] flex items-center justify-between md:mb-[30px] md:w-full">
-          <p className="hidden text-xs font-bold text-gray-500 md:flex">
+          <p className="flex hidden text-xs font-bold text-gray-500 md:block">
             Dash Boards
           </p>
-          <button className="relative h-5 w-5" type="button">
-            <Image src={addDashboard} alt="대시보드 추가 버튼" fill />
-          </button>
+          <SidebarButton />
         </div>
         {/* // NOTE - 대시보드 리스트 section */}
         <SidebarDashboardList />
