5ba77ef7d615cf55ce931e491a6db0442e023b25
seo02wow <rlatjdud9188@naver.com>
2024-06-29 15:53:11 +0900

    :lipstick:design: 메인 영역 너비 재조정

--- app/(dashboard)/mydashboard/page.tsx
+++ app/(dashboard)/mydashboard/page.tsx
@@ -3,8 +3,8 @@ import Invitations from './_components/invitations';
 
 export default function MyDashboard() {
   return (
-    <div className="md:w-[calc(100vw-160px)] xl:w-[calc(100vw-300px)]">
-      <main className="h-[calc(100vh-60px)] w-full overflow-auto p-6 md:h-[calc(100vh-70px)] md:p-10">
+    <div className="overflow-auto p-6 md:w-[calc(100vw-160px)] md:p-10 xl:w-[calc(100vw-300px)]">
+      <main className="h-[calc(100vh-60px)] w-full max-w-[1022px] md:h-[calc(100vh-70px)]">
         <DashBoards />
         <Invitations />
       </main>
