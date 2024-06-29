d2a6576566ac27008e4c679fa82cfb1f61212a45
CitrusSoda <dndhkaus1004@naver.com>
2024-06-29 22:33:02 +0900

    🐛 fix: shadcn 빌드 에러에 따른 eslint 규칙 무시 추가

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 "use client"
 
 import { useTheme } from "next-themes"
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+ /* eslint-disable */ 
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
