7df994e8c56e21a381846912ad972471768b1d13
seo02wow <rlatjdud9188@naver.com>
2024-07-01 12:10:40 +0900

    :sparkles:feat: 카드 생성 스키마 생성

---
+++ lib/schemas/createToDo.ts
@@ -0,0 +1,12 @@
+import * as yup from 'yup';
+
+export const createTodoSchema = yup.object().shape({
+  assigneeUserId: yup.number().required('담당자를 선택해 주세요.'),
+  title: yup
+    .string()
+    .required('제목을 입력해 주세요')
+    .max(10, '제목은 최대 10글자까지 입력 가능합니다.'),
+  description: yup.string().required('설명을 입력해 주세요'),
+  dueDate: yup.date().notRequired(),
+  tags: yup.array().of(yup.string()).notRequired(),
+});
