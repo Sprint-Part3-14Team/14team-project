0a654667b164e0751c3541ff875f85bb12061289
JHmeatschool <ceojr1234@gmail.com>
2024-07-01 21:40:08 +0900

    ✨ feat: 이메일 초대하기 기능 구현

--- app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/invite-button.tsx
+++ app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/invite-button.tsx
@@ -1,17 +1,24 @@
 'use client';
 
 import IconButtons from '@/app/components/icon-buttons';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import InviteModal from './invite-modal';
 
 export default function InviteButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const [currentDashboardId, setCurrentDashboardId] = useState<number>(-1);
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
-  // 여기만 해결되면 됨
-  const currentDashboardId = 9996;
+
+  useEffect(() => {
+    const pathSegments = window.location.pathname.split('/');
+    const dashboardId = pathSegments[pathSegments.length - 1];
+
+    if (!Number.isNaN(parseInt(dashboardId, 10))) {
+      setCurrentDashboardId(parseInt(dashboardId, 10));
+    }
+  }, []);
 
   return (
     <div>
--- app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/invite-modal.tsx
+++ app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/invite-modal.tsx
@@ -19,13 +19,17 @@ export default function InviteModal({
   const handleInviteByEmail = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const result = await Invitedashboard(dashboardId, inviteEmail);
-      console.log('초대 성공:', result);
+      // 추가된 로그
+      console.log('dashboardId:', dashboardId);
+      console.log('inviteEmail:', inviteEmail);
 
-      onClose();
-      setInviteEmail('');
+      const result = await Invitedashboard(dashboardId, inviteEmail);
+      console.log('초대 성공:', result); // 초대 성공 로그 출력
+      onClose(); // 초대 성공 시 모달 닫기
+      setInviteEmail(''); // 입력 필드 초기화
     } catch (error: any) {
       console.error('대시보드 초대 오류:', error.message);
+      // 오류 처리 로직 추가 가능
     }
   };
 
