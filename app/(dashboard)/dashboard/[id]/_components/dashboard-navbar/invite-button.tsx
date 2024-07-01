9a14b7bf3d6630eafcb126f13adf4c53cff6ef48
JHmeatschool <ceojr1234@gmail.com>
2024-07-01 15:44:01 +0900

    ✨ feat: 초대하기 버튼 생성

---
+++ app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/invite-button.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import IconButtons from '@/app/components/icon-buttons';
+
+export default function InviteButton() {
+  return (
+    <div>
+      <IconButtons variant="dashboard" />
+      {/* //NOTE - 초대하기 모달 추가 예정 */}
+    </div>
+  );
+}
