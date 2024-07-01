f0db4a8dd1a994b790da6e54a6074affea057142
CitrusSoda <dndhkaus1004@naver.com>
2024-07-01 12:16:29 +0900

    💄 design: 마이페이지 디자인 (#101)
    * 💄 design: 반응형 구현 및 유효성 검사 로직 수정
    
    * ✨ feat: 마이페이지 에러 토스트 구현
    
    * 💄 design: 마이페이지 디자인 마무리
    
    * 🐛 fix: shadcn 빌드 에러에 따른 eslint 규칙 무시 추가

--- app/(dashboard)/_components/edit-profile-form.tsx
+++ app/(dashboard)/_components/edit-profile-form.tsx
@@ -7,6 +7,7 @@ import { editProfileSchema } from '@/lib/schemas/profileUpdate';
 import { EditProfile } from '@/types/profileUpdate';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 
 import { editProfile } from '../mypage/actions';
 
@@ -26,7 +27,7 @@ export default function EditProfileForm() {
   const nicknameInput = watch('nickname');
 
   const isFormValid =
-    isValid && (profileImageUrlInput !== null || nicknameInput !== '');
+    isValid && (!!profileImageUrlInput || nicknameInput !== '');
 
   const onSubmit: SubmitHandler<EditProfile> = async (data) => {
     const { profileImageUrl, nickname } = data;
@@ -35,39 +36,44 @@ export default function EditProfileForm() {
     formData.append('image', profileImageUrl!);
 
     const res = await editProfile(formData, nickname);
-    // NOTE - 오류 메시지 출력
-    console.log(res);
+    // NOTE - 성공 메시지 출력
+    toast.success(res.message);
   };
 
   return (
     <div className="mt-6 rounded-lg bg-white p-5">
       <p className="text-xl font-bold md:text-2xl">프로필</p>
-      <form onSubmit={handleSubmit(onSubmit)} className="mt-6 md:mt-8">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-6 flex w-full flex-col md:mt-8 md:flex-row md:gap-x-4"
+      >
         <ImageInputField id="profileImageUrl" setValue={setValue} />
-        <InputField
-          id="email"
-          label="이메일"
-          type="email"
-          placeholder="현재 이메일"
-          register={register}
-          disabled
-        />
-        <InputField
-          id="nickname"
-          label="닉네임"
-          type="text"
-          placeholder="새 닉네임 입력"
-          register={register}
-          error={errors.nickname?.message || ''}
-        />
-        <Button
-          type="submit"
-          variant="mobile84x28"
-          className="ml-auto mt-4 rounded bg-violet-primary text-white disabled:cursor-not-allowed disabled:bg-gray-400"
-          disabled={!isFormValid}
-        >
-          저장
-        </Button>
+        <div className="flex w-full flex-col">
+          <InputField
+            id="email"
+            label="이메일"
+            type="email"
+            placeholder="이메일"
+            register={register}
+            disabled
+          />
+          <InputField
+            id="nickname"
+            label="닉네임"
+            type="text"
+            placeholder="새 닉네임 입력"
+            register={register}
+            error={errors.nickname?.message || ''}
+          />
+          <Button
+            type="submit"
+            variant="mobile84x28"
+            className="ml-auto mt-4 rounded bg-violet-primary text-white disabled:cursor-not-allowed disabled:bg-gray-400"
+            disabled={!isFormValid}
+          >
+            저장
+          </Button>
+        </div>
       </form>
     </div>
   );
--- app/components/image-input-field.tsx
+++ app/components/image-input-field.tsx
@@ -26,7 +26,7 @@ export default function ImageInputField({
   return (
     <div>
       {selectedImage ? (
-        <div className="relative size-[100px]">
+        <div className="relative size-[100px] md:size-[182px]">
           <Image
             src={URL.createObjectURL(selectedImage)}
             alt="Selected Image"
@@ -45,7 +45,10 @@ export default function ImageInputField({
       ) : (
         // NOTE - label이 form안에 직접적으로 있어야 하나 x button 동작으로 다시 한번 input되는 문제가 발생하여 eslint disable처리
         // eslint-disable-next-line jsx-a11y/label-has-associated-control
-        <label htmlFor={id} className="inline-block size-[100px]">
+        <label
+          htmlFor={id}
+          className="relative inline-block size-[100px] md:size-[182px]"
+        >
           <input
             id={id}
             type="file"
@@ -53,7 +56,15 @@ export default function ImageInputField({
             onChange={(e) => handleImageChange(e)}
             className="hidden"
           />
-          <div className="size-[100px] cursor-pointer rounded-md bg-gray-200" />
+          <div className="absolute left-1/2 top-1/2 size-5 -translate-x-1/2 -translate-y-1/2 md:size-[30px]">
+            <Image
+              src="/icons/icon_purple_add.svg"
+              alt="Add Image"
+              sizes="(min-width: 768px) 100vw"
+              fill
+            />
+          </div>
+          <div className="size-[100px] cursor-pointer rounded-md bg-gray-200 md:size-[182px]" />
         </label>
       )}
     </div>
