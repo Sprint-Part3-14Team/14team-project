e402930400ad8eb7571db95e408836b155cf4ffe
haze <skdml20022@gmail.com>
2024-07-02 21:36:50 +0900

    🐛 fix: 대시보드 링크 추가

--- app/(dashboard)/mydashboard/_components/dashboard-card.tsx
+++ app/(dashboard)/mydashboard/_components/dashboard-card.tsx
@@ -1,6 +1,7 @@
 import ProfileImage from '@/app/components/profile/profile-image';
 import { DashboardDetail } from '@/types/dashboard';
 import { DashboardMembers } from '@/types/members';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 import { getDashboardMember } from '../actions';
@@ -25,23 +26,25 @@ export default function DashboardCard({ dashboard }: DashboardProps) {
   }, []);
 
   return (
-    <li
-      className="relative flex h-20 items-center justify-between rounded-lg border border-gray-300 bg-white px-5 py-5"
-      key={dashboard.id}
-    >
-      <div
-        className="absolute left-0 top-[50%] h-7 w-1 translate-y-[-50%] rounded-r-lg"
-        style={{ backgroundColor: dashboard.color }}
-      />
-      <p className="text-sm font-semibold text-gray-700">{dashboard.title}</p>
-      {ownerUser && (
-        <ProfileImage
-          nickname={ownerUser.nickname}
-          profileImageUrl={ownerUser.profileImageUrl}
-          id={ownerUser.userId}
-          size="20px"
+    <li key={dashboard.id}>
+      <Link
+        href={`/dashboard/${dashboard.id}`}
+        className="relative flex h-20 items-center justify-between rounded-lg border border-gray-300 bg-white px-5 py-5"
+      >
+        <div
+          className="absolute left-0 top-[50%] h-7 w-1 translate-y-[-50%] rounded-r-lg"
+          style={{ backgroundColor: dashboard.color }}
         />
-      )}
+        <p className="text-sm font-semibold text-gray-700">{dashboard.title}</p>
+        {ownerUser && (
+          <ProfileImage
+            nickname={ownerUser.nickname}
+            profileImageUrl={ownerUser.profileImageUrl}
+            id={ownerUser.userId}
+            size="20px"
+          />
+        )}
+      </Link>
     </li>
   );
 }
