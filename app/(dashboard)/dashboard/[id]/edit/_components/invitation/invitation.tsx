f2c45fbd028a28afefc36299553a38be25de511a
haze <skdml20022@gmail.com>
2024-07-02 14:01:40 +0900

    🐛 fix: eslint 수정

--- app/(dashboard)/dashboard/[id]/edit/_components/invitation/invitation.tsx
+++ app/(dashboard)/dashboard/[id]/edit/_components/invitation/invitation.tsx
@@ -1,5 +1,6 @@
 import { getInvitation } from '../../actions';
 import SectionTitle from '../section-title';
+import InvitationButton from './invitation-button';
 import InvitationList from './invitation-list';
 
 export default async function Invitation({
@@ -15,6 +16,7 @@ export default async function Invitation({
       <div className="px-5 pt-6 md:px-7 md:pt-8">
         <SectionTitle sectionTitle="초대 내역" listTitle="이메일" />
       </div>
+      <InvitationButton />
       <InvitationList dashboardId={dashboardId} initialData={invitations} />
     </section>
   );
