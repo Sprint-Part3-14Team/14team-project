c96ef341bb6488cc0bdc171cfff16a43366059fa
seo02wow <rlatjdud9188@naver.com>
2024-07-04 23:12:04 +0900

    :lipstick:design: 초대받은 대시보드 없는 경우 디자인 적용

--- app/(dashboard)/mydashboard/_components/invitations.tsx
+++ app/(dashboard)/mydashboard/_components/invitations.tsx
@@ -1,5 +1,6 @@
 import { INITIAL_NUMBER_OF_USERS } from '@/constants/TEAM_BASE_URL';
 import search from '@/public/icons/search.svg';
+import invitation from '@/public/images/none_invitation.svg';
 import { InvitationResponse } from '@/types/invitations';
 import Image from 'next/image';
 
@@ -14,24 +15,39 @@ export default async function Invitations() {
   const { invitations, cursorId } = data;
 
   return (
-    <section className="mt-6 bg-white px-4 py-6 md:px-7 md:py-8">
+    <section className="mt-6 w-full bg-white px-4 py-6 md:px-7 md:py-8">
       <h2 className="mb-5 text-xl font-bold text-gray-700">
         초대받은 대시보드
       </h2>
-      <div className="relative">
-        <input
-          type="text"
-          className="h-9 w-full rounded-md border border-gray-300 py-[10px] pl-[44px] placeholder:text-sm placeholder:text-gray-400"
-          placeholder="검색"
-        />
-        <div className="absolute left-[20px] top-[50%] translate-y-[-50%]">
-          <Image src={search} width={16} height={16} alt="검색" />
+      {invitations.length === 0 ? (
+        <div className="h-[352px] w-full">
+          <div className="flex h-full flex-col items-center justify-center gap-4">
+            <div className="relative size-[60px] md:size-[100px]">
+              <Image src={invitation} fill alt="초대받은 대시보드 없음" />
+            </div>
+            <p className="text-sm font-normal text-gray-400 md:text-lg">
+              아직 초대받은 대시보드가 없어요
+            </p>
+          </div>
         </div>
-      </div>
-      <InvitationList
-        initialInvitations={invitations}
-        initialCursorId={cursorId}
-      />
+      ) : (
+        <>
+          <div className="relative">
+            <input
+              type="text"
+              className="h-9 w-full rounded-md border border-gray-300 py-[10px] pl-[44px] placeholder:text-sm placeholder:text-gray-400"
+              placeholder="검색"
+            />
+            <div className="absolute left-[20px] top-[50%] translate-y-[-50%]">
+              <Image src={search} width={16} height={16} alt="검색" />
+            </div>
+          </div>
+          <InvitationList
+            initialInvitations={invitations}
+            initialCursorId={cursorId}
+          />
+        </>
+      )}
     </section>
   );
 }
