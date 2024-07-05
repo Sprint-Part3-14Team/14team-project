09fedd725017bf21fc920f80e00678f294be97e2
JHmeatschool <162143999+JHmeatschool@users.noreply.github.com>
2024-07-05 13:58:06 +0900

    💄design: color-picker 기능 추가  (#151)
    * ✨ feat: color picker 추가
    
    * ♻️ refactor: 파일 수정
    
    * ♻️ refactor: 파일 추가 수정

---
+++ app/components/color-picker.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface ColorPickerProps {
+  value: string;
+  onChange: (color: string) => void;
+  disabled?: boolean;
+}
+
+export default function ColorPicker({
+  value,
+  onChange,
+  disabled = false,
+}: ColorPickerProps): JSX.Element {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="color"
+        className="h-[30px] w-[30px] cursor-pointer appearance-none rounded-full border border-gray-400 bg-white p-0.5"
+        id="hs-color-input"
+        value={value}
+        onChange={handleChange}
+        disabled={disabled}
+        title="색상 선택"
+      />
+    </div>
+  );
+}
