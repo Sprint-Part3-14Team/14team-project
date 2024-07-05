e7b9a7ea946f8c5111af63617aa25ff25af214e2
JHmeatschool <162143999+JHmeatschool@users.noreply.github.com>
2024-07-05 22:59:50 +0900

    🐛 fix: 대시보드 초대하기 에러처리   (#155)
    * ✨ feat: 초대하기 에러처리 추가
    
    * ✨ feat: 초대하기 에러 메세지 수정
    
    * ♻ refactor: 이메일 GET 코드 수정

--- lib/schemas/inviteEmail.ts
+++ lib/schemas/inviteEmail.ts
@@ -1,10 +1,15 @@
+import { MyEmail } from '@/app/(dashboard)/dashboard/[id]/_components/dashboard-navbar/actions';
 import * as yup from 'yup';
 
 const inviteEmailSchema = yup.object().shape({
   email: yup
     .string()
     .email('이메일 형식으로 작성해 주세요.')
-    .required('이메일을 입력해 주세요.'),
+    .required('이메일을 입력해 주세요.')
+    .test('is-self-email', '본인은 초대할 수 없습니다.', async (value) => {
+      const myEmail = await MyEmail();
+      return value !== myEmail;
+    }),
 });
 
 export default inviteEmailSchema;
