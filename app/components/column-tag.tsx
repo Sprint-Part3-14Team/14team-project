bac911d8b5bce42ca39882d7ea71a989ae91043f
seo02wow <rlatjdud9188@naver.com>
2024-07-05 17:56:26 +0900

    :lipstick:design: 컬럼 태그 디자인 수정

--- app/components/column-tag.tsx
+++ app/components/column-tag.tsx
@@ -1,6 +1,6 @@
 export default function ColumnTag({ title }: { title: string }) {
   return (
-    <div className="flex h-[20px] w-[55px] items-center justify-center gap-x-[6px] rounded-[11px] bg-violet-primary/10 md:h-[22px] md:w-[60px]">
+    <div className="flex h-[20px] items-center justify-center gap-x-[6px] rounded-[11px] bg-violet-primary/10 px-[8px] py-[4px] md:h-[22px]">
       <div className="size-[6px] rounded-full bg-violet-primary" />
       <p className="text-[10px] text-violet-primary md:text-xs">{title}</p>
     </div>
