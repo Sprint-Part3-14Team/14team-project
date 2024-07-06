d97950093cb71c4aa95cee62eb078d0943d9c1b7
JHmeatschool <162143999+JHmeatschool@users.noreply.github.com>
2024-07-06 09:18:14 +0900

    ♻️ refactor: color-radio 수정

--- app/(dashboard)/dashboard/[id]/edit/_components/dashboard/color-radio.tsx
+++ app/(dashboard)/dashboard/[id]/edit/_components/dashboard/color-radio.tsx
@@ -16,7 +16,7 @@ export default function ColorRadio({
   return (
     <label>
       <input
-        {...register('color')}
+        {...register('color1')}
         className={`h-[30px] w-[30px] cursor-pointer appearance-none rounded-full bg-center checked:bg-[url('/icons/checked.svg')] ${cssColor}`}
         type="radio"
         name="color"
