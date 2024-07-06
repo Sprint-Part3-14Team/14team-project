80509f356ca55ecc3caf943ff4243f1fb6b7082c
JHmeatschool <162143999+JHmeatschool@users.noreply.github.com>
2024-07-06 09:13:18 +0900

    ♻️ refactor: color picker 수정

--- app/components/color-picker.tsx
+++ app/components/color-picker.tsx
@@ -8,7 +8,7 @@ export default function ColorPicker({ register }: ColorPickerProps) {
   return (
     <div>
       <input
-        {...register('color')}
+        {...register('color2')}
         type="color"
         className="h-[30px] w-[30px] cursor-pointer appearance-none rounded-full border border-gray-400 bg-white p-0.5"
         id="hs-color-input"
