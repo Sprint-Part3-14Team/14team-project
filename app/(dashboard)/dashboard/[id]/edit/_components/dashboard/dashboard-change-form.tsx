6e03ee2a8fd91419b89c19820778c7bbc421e17b
JHmeatschool <ceojr1234@gmail.com>
2024-07-07 01:23:52 +0900

    ♻ refactor: 컬러피커 추가 및 사이드바 업데이트

--- app/(dashboard)/dashboard/[id]/edit/_components/dashboard/dashboard-change-form.tsx
+++ app/(dashboard)/dashboard/[id]/edit/_components/dashboard/dashboard-change-form.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import ColorList from '@/app/components/color-list';
+import ColorPicker from '@/app/components/color-picker';
 import { Dashboard } from '@/types/dashboard';
+import { useRouter } from 'next/navigation';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 import putDashboardInfo from '../../actions';
@@ -23,10 +25,15 @@ export default function DashboardChangeForm({
       color: dashboardColor,
     },
   });
+  const router = useRouter();
 
   const changeDashboardInfo: SubmitHandler<Dashboard> = async (data) => {
     const { title, color } = data;
     await putDashboardInfo(title, color, dashboardId);
+    router.push(`/dashboard/${dashboardId}/edit`);
+    setTimeout(() => {
+      window.location.reload();
+    }, 400);
   };
 
   const handleColorChange = (color: string) => {
@@ -36,12 +43,21 @@ export default function DashboardChangeForm({
   return (
     <div>
       <form className="mt-6" onSubmit={handleSubmit(changeDashboardInfo)}>
-        <ColorList
-          setValue={setValue}
-          onColorChange={handleColorChange}
-          className="right-7 top-11"
-          register={register}
-        />
+        <div className="flex justify-end">
+          <ColorList
+            setValue={setValue}
+            onColorChange={handleColorChange}
+            className="right-[61px] md:right-[73px]"
+            register={register}
+          />
+          <div className="right-[12px] md:right-[5px]">
+            <ColorPicker
+              register={register}
+              setValue={setValue}
+              onColorChange={handleColorChange}
+            />
+          </div>
+        </div>
         <label htmlFor="title">대시보드 이름</label>
         <input
           {...register('title')}
