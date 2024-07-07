7826383eb3b7f203e090ee4471084e712ace7acc
seo02wow <rlatjdud9188@naver.com>
2024-07-07 15:42:48 +0900

    :lipstick:design: 사이드바 mydashboard 버튼 변경

--- app/components/sidebar/sidebar-button.tsx
+++ app/components/sidebar/sidebar-button.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import NewDashboardModal from '@/app/(dashboard)/mydashboard/_components/new-dashboard-modal';
+import clipboard from '@/public/images/clipboard.svg';
+import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 
 export default function SidebarButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const pathname = usePathname();
+
+  const isMydashboardPage = pathname === '/mydashboard';
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -29,10 +35,12 @@ export default function SidebarButton() {
       {/* NOTE - 나의 대시보드 페이지 */}
       <Link href="/mydashboard">
         <button
-          className="mb-[16px] size-[50px] rounded-full bg-orange-200 text-center text-xl font-semibold text-primary"
+          className={`mb-[16px] flex size-[50px] items-center justify-center rounded-full bg-[#5A5A5A] text-center hover:bg-primary/35 ${isMydashboardPage && 'rounded-md bg-primary/70'}`}
           type="button"
         >
-          +
+          <div className="relative size-[22px]">
+            <Image src={clipboard} fill alt="마이대시보드로 가는 버튼" />
+          </div>
         </button>
       </Link>
     </>
